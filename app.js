504db99b3396c47413685088877ec30d9122b93a
Steve Fox <stevefox.dev@gmail.com>
2025-04-27 18:53:22 -0400

    add new domain to cors options

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const app = express();
 
 const corsOptions = {
-    origin: ['http://localhost'],
+    origin: ['https://www.stevefox.dev'],
     allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Methods", "Access-Control-Request-Headers"],
     credentials: true,
     enablePreflight: true
