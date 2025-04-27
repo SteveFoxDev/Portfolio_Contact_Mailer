96ed6510989c03f177f34f91e8784f2ed6bae319
Steve Fox <stevefox.dev@gmail.com>
2025-04-27 18:48:59 -0400

    cors config placement change

--- app.js
+++ app.js
@@ -8,12 +8,18 @@ const cors = require('cors');
 
 const app = express();
 
+const corsOptions = {
+    origin: ['http://localhost'],
+    allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Methods", "Access-Control-Request-Headers"],
+    credentials: true,
+    enablePreflight: true
+}
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions))
+
 app.use(express.urlencoded({ extended: true })); // Parse Body Data
 app.use(express.json()); // Parse incoming JSON body data
-
-//Cross origin setup
-const URL = process.env.CORS_ORIGIN || 'http://localhost:5173';
-app.options('*', cors());
 
 //Mailer config
 const transporter = nodemailer.createTransport({
